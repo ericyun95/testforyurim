d828276d71c55cfc100c56914dd8a3862f18f1a3
Yun Hyunwooong <59863547+ericyun95@users.noreply.github.com>
2024-08-01 18:38:21 +0900

    Add files via upload

--- Joseon Dynasty Silok Scraper.py
+++ Joseon Dynasty Silok Scraper.py
@@ -30,3 +30,15 @@ driver.quit()
 # 2024/8/1 크롬드라이버 정상작동 확인
 # 여기서부터 한국고전종합DB - https://db.itkc.or.kr/
 # 크롬드라이버 사용이유 = API 사용시 전문 확보 불가
+
+
+url = 'https://db.itkc.or.kr/'
+driver.get(url)
+
+# 사이트 로드 대기
+time.sleep(3)  # 페이지가 완전히 로드될 때까지 잠시 대기
+
+# 조선왕조실록으로 이동
+driver.find_element(By.LINK_TEXT, '조선왕조실록').click()
+time.sleep(3)
+# dataId=ITKC_JT_A0 이런식으로 이어지는것 발견, 확인해서 쓰면 좋을듯.
